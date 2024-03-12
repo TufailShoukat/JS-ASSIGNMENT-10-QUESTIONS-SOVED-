b0b0ebe06f7740d905a2be5bbd9a74da33dd34ad
Tufail Shoukat <154624057+TufailShoukat@users.noreply.github.com>
2024-03-12 14:23:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,270 @@
+//JS ASSIGNMAEMT (10) QUESTIONS!
+
+// !=======Q:1=============!EVEN/ODD NUMBER CHECKER: 
+
+// let input1 =  prompt("Enter the Number Here")
+// if ( input1== 2   ) {
+//     document.write("<h1> The Number is <u> Even</u>.!      </h1>")
+
+// }
+// else {
+//    document.write ("<h1> The Number is <u> Odd</u>.!")
+// }
+
+
+
+// //!=======Q:2=============!POSTIVE/NEGAIVE NUMBER CHECKER:
+// var userinput1 = prompt ("Enter the Number here! ");
+//      if( userinput1 == +2   )
+//      {
+//         document.write ("<h1>The Number is <u> Postive</u>.!</h1> ");
+//       }
+
+//    else {      
+//     document.write("<h1> The Number is <u> Negative</u>.!</h1> </h1>");
+//        }
+//     else if( userinput1 == 0 )
+
+//     document.write("<h1> The Number is <u> Zero</u>.!</h1> </h1>");
+
+
+
+// //!=======Q:3=============!Maximum of two Numbers:
+//  var input3 = prompt("Enter the Two Number!");
+//  if(  input3              )
+//  document.write("<h1>Large Number!</h1>")
+
+
+
+
+
+// //!=======Q:4=============!Maximum of three Numbers:
+//  var input4 = prompt("Enter the Three Number!");
+//  if(  input4             )
+//  document.write("<h1>Smallest Number!</h1>")
+
+
+
+// //!=======Q:5=============Grade Calcuate:
+// var userinput5 = prompt ("Enter the Marks Here English's out of (100) ")
+// var userinput6 =   prompt ("Enter the Marks Here Math's! out of (100) ")
+// var userinput6 =   prompt ("Enter the Marks Here Sceince's!  out of (100)")
+// var userinput7 =   prompt ("Enter the here total Passed Marks Out of (300)")
+
+
+//      if( userinput5 >= 80  && userinput5 < 100 )
+//      {
+//         document.write ("<h1>Proud of yourself (A+)Grade</h1> ")
+
+//      } 
+
+//      else if( userinput5 >= 70 && userinput5 <80 )
+
+//          document.write("<h1>(A) grade </h1>")
+
+//          else if( userinput5 >= 60 && userinput5 <70 )
+
+//          document.write("<h1>(B) grade </h1>")
+
+//          else if( userinput5 >= 50 && userinput5 <60 )
+
+//          document.write("<h1>(C)  Grade </h1>")
+
+
+//          else if( userinput5 >=  40 && userinput5 <50 )
+
+//          document.write("<h1>(D)  Grade </h1>")
+//          else if( userinput5 >= 30 && userinput5 <40 )
+
+//          document.write("<h1>(E) grade </h1>")
+
+//          else if( userinput5 >= 0 && userinput5 <30 )
+
+//          document.write("<h1> SORRY! You failed try again. </h1>")
+
+
+
+// // //!=======Q:6============= Temperature Q:6:
+// prompt("Enter a Temperature in celsius ")
+// function convertToFahrenheitToCelsius(fahrenheit) {
+//     return (fahrenheit - 32) * 5/9;
+// }
+
+// // Example temperature in Fahrenheit
+// var temperatureInFahrenheit = 98.6;
+
+// // Convert temperature to Celsius
+// var temperatureInCelsius = convertToFahrenheitToCelsius(temperatureInFahrenheit);
+
+// // Display the result using an alert
+// document.write("Temperature in ferhreneit: " + temperatureInCelsius.toFixed(2));
+
+// // //!=======Q:7=============:
+// var abc = prompt("Enter the character here! & check it Vowel or consonat ");
+
+
+
+// if (abc == "A") {
+//     document.write("<h1>It is <u>Vowel</u> Character! </h1>")
+// }
+// else{
+//     document.write(" <h1>It is <u>Consonant</u> Character!</h1>")
+// }
+
+
+
+// if (abc == "E") {
+//     document.write("<h1>It is <u>Vowel</u> Character! </h1>")
+// } 
+
+
+// if (abc == "I") {
+//     document.write("<h1>It is <u>Vowel</u> Character! </h1>")
+// } 
+
+
+// if (abc == "O") {
+//     document.write("<h1>It is <u>Vowel</u> Character! </h1>")
+// } 
+
+
+
+// if (abc == "U") {
+//     document.write("<h1>It is <u>Vowel</u> Character! </h1>")
+// } 
+
+// } 
+
+// else{
+//     ( "<h1>It is <u>Consonat</u> Character! </h1>")
+// }
+
+
+// else if (abc = "a")
+
+//     document.write("<h1> It is <u>Vowel</u> Character! </h1>")
+
+
+// else if (abc = "e")
+
+//     document.write("<h1> It is <u>Vowel</u> Character! </h1>")
+
+
+// else if (abc = "i")
+
+//     document.write("<h1> It is <u>Vowel</u> Character! </h1>")
+
+// else if (abc = "o")
+
+//     document.write("<h1> It is <u>Vowel</u> Character! </h1>")
+
+
+// else if (abc = "u")
+
+//     document.write("<h1> It is <u>Vowel</u> Character! </h1>")
+
+
+
+
+// // //!=======Q:8=============:
+// var exp = prompt("Enter the Base Number with its Exponet!")
+// if(exp==  4*2 ){
+//         document.write(12)
+// }
+
+
+
+    
+
+// // //!=======Q:9=============:
+// var email = prompt("Enter the Email Here!");
+// var password = prompt(" Enter the Password Here!");
+// if (email =="tufail" && password == "1122") {
+//     document.write("<h1> Login Successfully Here!<h1> ")
+    
+// }
+// else{  document.write          (" <h1>Invalid  Password or Email! </h1>")
+// }
+    
+
+// // //!=======Q:10=============:CHECK THE AGE!
+//  var age = prompt("Enter the Age!")
+//  if (age >= 0  && age < 13) {
+//     document.write("<H1>(CHILD!)</H1>")
+    
+//  }
+
+//  else if( age  >=14 && age<19 )
+
+//            document.write("<h1> (Teenger!) </h1>")
+
+           
+//  else if( age => 20 && age<65 )
+
+//  document.write("<h1> (Adult!) </h1>")
+ 
+//  else if( age >= 65 && age<1000 )
+
+//            document.write("<h1> (Senior!) </h1>")
+
+
+     //  ==!!!!!!!!!!!///THE END (10)QUESTIONS SOLVED==!!!!!!!!!!!!1!///
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
